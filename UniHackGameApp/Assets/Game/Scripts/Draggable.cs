449528ffcefda0b5754a1bd0a695863dc9269d3e
Gabriel Turcin <gabriel10tm@gmail.com>
2024-11-09 16:13:39 +0200

    fixed dragging

--- UniHackGameApp/Assets/Game/Scripts/Draggable.cs
+++ UniHackGameApp/Assets/Game/Scripts/Draggable.cs
@@ -7,13 +7,14 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] bool returnToPos = true;
+    [SerializeField] bool allowDragChildren = false;
     public Action onDropped { get; set; } = () => { };
 
     RectTransform rectTransform;
     CanvasGroup canvasGroup;
     Vector2 originalPosition;
     Canvas canvas;
-
+    bool cancelThisDrag = false;
 
     void Awake()
     {
@@ -29,6 +30,14 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+
+        if (allowDragChildren == false)
+        {
+            cancelThisDrag = IsPointerOverGameObject(eventData) == false;
+            if (cancelThisDrag)
+                return;
+        } 
+
         // Save original position in case of cancel
         originalPosition = rectTransform.anchoredPosition;
         // Reduce the opacity to indicate dragging
@@ -39,12 +48,16 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (cancelThisDrag)
+            return;
         // Move the item along with the mouse pointer
         rectTransform.anchoredPosition += eventData.delta;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (cancelThisDrag)
+            return;
         // Reset opacity and enable raycasts again
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
@@ -58,5 +71,12 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
             rectTransform.anchoredPosition = originalPosition;
         }
 
+    }
+
+
+    private bool IsPointerOverGameObject(PointerEventData eventData)
+    {
+        // Check if the pointer is over the current GameObject
+        return eventData.pointerEnter == gameObject;
     }
 }
